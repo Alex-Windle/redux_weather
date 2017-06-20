492e6906541ac3566eeea90efc907678634232ca
Alex Windle <Alex-Windle@users.noreply.github.com>
2017-06-20 08:59:33 -0400

    Clean up code.

--- src/App.jsx
+++ src/App.jsx
@@ -4,54 +4,13 @@ import 'bootstrap/dist/css/bootstrap.css';
 import { Navbar, NavItem, Nav, Grid, Row, Col } from "react-bootstrap";
 import "bootswatch/flatly/bootstrap.css";
 
-const PLACES = [
-  { name: "Palo Alto", zip: "94303" },
-  { name: "San Jose", zip: "94088" },
-  { name: "Santa Cruz", zip: "95062" },
-  { name: "Honolulu", zip: "96803" }
-];
+import PLACES from './places'; 
 
-class WeatherDisplay extends Component {
-  constructor() {
-    super();
-    this.state = {
-      weatherData: null
-    };
-  }
-
-  componentDidMount() {
-    const zip = this.props.zip;
-    const URL = "http://api.openweathermap.org/data/2.5/weather?q=" +
-      zip +
-      "&appid=b1b35bba8b434a28a0be2a3e1071ae5b&units=imperial";
-    fetch(URL).then(res => res.json()).then(json => {
-      this.setState({ weatherData: json });
-    });
-  }
-  
-  render() {
-    const weatherData = this.state.weatherData;
-    if (!weatherData) return <div>Loading</div>;
-    const weather = weatherData.weather[0];
-    const iconUrl = "http://openweathermap.org/img/w/" + weather.icon + ".png";
-    return (
-      <div>
-        <h1>
-          {weather.main} in {weatherData.name}
-          <img src={iconUrl} alt={weatherData.description} />
-        </h1>
-        <p>Current: {weatherData.main.temp}°</p>
-        <p>High: {weatherData.main.temp_max}°</p>
-        <p>Low: {weatherData.main.temp_min}°</p>
-        <p>Wind Speed: {weatherData.wind.speed} mi/hr</p>
-      </div>
-    );
-  }
-}
+import WeatherDisplay from './WeatherDisplay'; 
 
 class App extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
     this.state = { activePlace: 0 }
   }
