029d2e41cf89772cd59a68b4e2037f6f8fd84da5
Alex Windle <Alex-Windle@users.noreply.github.com>
2017-07-05 10:46:52 -0400

    Update data to reflect relevant places.

--- src/data/places.js
+++ src/data/places.js
@@ -1,8 +1,8 @@
 const PLACES = [
-  { name: "Marietta", zip: "30067" },
-  { name: "Port Royal", zip: "29902" },
-  { name: "Bronx", zip: "10453" },
-  { name: "Honolulu", zip: "96803" }
+  { name: "Marietta, GA", zip: "30067" },
+  { name: "West Midtown, GA", zip: "30309" },
+  { name: "Port Royal, SC", zip: "29902" },
+  { name: "Queens, NYC", zip: "11691" }
 ];
 
 export default PLACES;
